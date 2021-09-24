f1671d6fe24d9d507236664779171ebeda2966c4
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-09-24 15:55:45 -0500

    create second story board for Zicheng Geng

---
+++ FunTwo/GViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GViewController.swift
+//  FunTwo
+//
+//  Created by Amor on 2021/9/24.
+//
+
+import UIKit
+
+// As a ViewController of the SecondVC view in the Second Story Board
+class GViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
