d5d024532f11e3580f9baa86613022eed226350e
amor-tsai <8969139+amor-tsai@users.noreply.github.com>
2021-09-24 17:31:28 -0500

    add AudioDSPUtils into the project

---
+++ FunTwo/AudioModel.swift
@@ -0,0 +1,158 @@
+//
+//  AudioModel.swift
+//  AudioLabSwift
+//
+//  Created by Eric Larson 
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+import Foundation
+import Accelerate
+
+class AudioModel {
+    
+    // MARK: Properties
+    private var BUFFER_SIZE:Int
+    // thse properties are for interfaceing with the API
+    // the user can access these arrays at any time and plot them if they like
+    var timeData:[Float]
+    var fftData:[Float]
+    var fft20MaximumData:[Float]
+    var stride:Int
+    var fft20Count:Int
+    
+    // MARK: Public Methods
+    init(buffer_size:Int,fft20:Int) {
+        BUFFER_SIZE = buffer_size
+        // anything not lazily instatntiated should be allocated here
+        timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
+        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
+        fft20MaximumData = Array.init(repeating: 0.0, count: 20)
+        fft20Count = fft20
+        stride = fftData.count/fft20Count
+    }
+    
+    // public function for starting processing of microphone data
+    func startMicrophoneProcessing(withFps:Double){
+        // setup the microphone to copy to circualr buffer
+        if let manager = self.audioManager{
+            manager.inputBlock = self.handleMicrophone
+            
+            // repeat this fps times per second using the timer class
+            //   every time this is called, we update the arrays "timeData" and "fftData"
+            Timer.scheduledTimer(timeInterval: 1.0/withFps, target: self,
+                                 selector: #selector(self.runEveryInterval),
+                                 userInfo: nil,
+                                 repeats: true)
+        }
+    }
+    
+    func startAudioPlayProcessing() {
+        if let manager = self.audioManager{
+            manager.outputBlock = self.handleSpeakerQueryWithAudioFile
+        }
+    }
+    
+    // You must call this when you want the audio to start being handled by our model
+    func play(){
+        if let manager = self.audioManager{
+            manager.play()
+        }
+        print("it's playing")
+    }
+    
+    // call this when you want the audio to pause being handled by this model
+    func pause(){
+        if let manager = self.audioManager{
+            manager.pause()
+        }
+        print("it stops playing")
+    }
+    
+    //==========================================
+    // MARK: Private Properties
+    private lazy var audioManager:Novocaine? = {
+        return Novocaine.audioManager()
+    }()
+    
+    private lazy var fftHelper:FFTHelper? = {
+        return FFTHelper.init(fftSize: Int32(BUFFER_SIZE))
+    }()
+    
+    
+    private lazy var inputBuffer:CircularBuffer? = {
+        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
+                                   andBufferSize: Int64(BUFFER_SIZE))
+    }()
+    
+    //to load mp3 file
+    private lazy var fileReader:AudioFileReader? = {
+        if let url = Bundle.main.url(forResource: "satisfaction", withExtension: "mp3"){
+            var tmpFileReader:AudioFileReader? = AudioFileReader.init(audioFileURL: url,
+                                                                        samplingRate: Float(audioManager!.samplingRate),
+                                                                        numChannels: audioManager!.numOutputChannels)
+            tmpFileReader!.currentTime = 0.0
+            print("Audio file succesfully loaded for \(url)")
+            return tmpFileReader
+        }else {
+            print("Audio file failed to load")
+            return nil
+        }
+    }()
+    
+    
+    //==========================================
+    // MARK: Private Methods
+    // NONE for this model
+    
+    //==========================================
+    // MARK: Model Callback Methods
+    @objc
+    private func runEveryInterval(){
+        if inputBuffer != nil {
+            // copy time data to swift array
+            self.inputBuffer!.fetchFreshData(&timeData,
+                                             withNumSamples: Int64(BUFFER_SIZE))
+            
+            // now take FFT
+            fftHelper!.performForwardFFT(withData: &timeData,
+                                         andCopydBMagnitudeToBuffer: &fftData)
+            
+            // at this point, we have saved the data to the arrays:
+            //   timeData: the raw audio samples
+            //   fftData:  the FFT of those same samples
+            // the user can now use these variables however they like
+            
+            
+            //calculate the values of this new array by looping through the FFT magnitude array to take maxima of windows
+            
+            for i in 0..<fft20Count {
+//                let arraySlice = fftData[i*stride..<(i+1)*stride]
+//                let tempArr = Array(arraySlice)
+//                let tempMax = vDSP.maximum(tempArr)
+//                fft20MaximumData[i] = tempMax
+                
+                vDSP_maxv(Array(fftData[i*stride..<(i+1)*stride]), 1, &fft20MaximumData[i], vDSP_Length(self.stride))
+                
+            }
+            
+
+        }
+    }
+    
+    //==========================================
+    // MARK: Audiocard Callbacks
+    // in obj-C it was (^InputBlock)(float *data, UInt32 numFrames, UInt32 numChannels)
+    // and in swift this translates to:
+    private func handleMicrophone (data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
+        // copy samples from the microphone into circular buffer
+        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
+    }
+    
+    private func handleSpeakerQueryWithAudioFile(data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
+        if let file = self.fileReader {
+            file.retrieveFreshAudio(data, numFrames: numFrames, numChannels: numChannels)
+        }
+    }
+    
+}
